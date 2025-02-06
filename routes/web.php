0cd42132865775fcdc7b1cd6cf6c421045b3ef31
Raisul Islam <raisulraat7@gmail.com>
2025-02-06 13:04:01 +0600

    uploade first time

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/admin', function () {
+    return view('admin');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
